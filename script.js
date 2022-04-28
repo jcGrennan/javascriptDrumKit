a9f0e39701770a38f07c2d93509fff1d9e7aea19
JC Grennan <jgrennan94@gmail.com>
2022-04-28 22:33:06 +0100

    tidied

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-window.addEventListener("keydown", function(ev) {
+function playSound(ev) {
     const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${ev.keyCode}"s]`)
+    const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`)
     if(!audio) return
     audio.currentTime = 0
     audio.play()
     key.classList.add("playing")
-})
+}
 
 function removeTransition(ev) {
     if(ev.propertyName !== "transform") return
     this.classList.remove("playing")
 }
+
 const keys = document.querySelectorAll(".key")
 keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+window.addEventListener("keydown", playSound)
