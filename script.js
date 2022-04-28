3fd047b5d79014235d9d96c66a4e5fdd0186acbf
JC Grennan <jgrennan94@gmail.com>
2022-04-28 22:26:09 +0100

    improved drumkit. readying it for css

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 window.addEventListener("keydown", function(ev) {
     const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${ev.keyCode}"s]`)
     if(!audio) return
+    audio.currentTime = 0
     audio.play()
+    key.classList.add("playing")
 })
+
+function removeTransition(ev) {
+    if(ev.propertyName !== "transform") return
+    this.classList.remove("playing")
+}
+const keys = document.querySelectorAll(".key")
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
